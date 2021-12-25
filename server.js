268f02bb07091908873a65f4843c1fd28f705cbd
Diky M <dmulyana@gmail.com>
2021-12-25 17:48:34 +0700

    update express module

--- server.js
+++ server.js
@@ -4,10 +4,19 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+// Routes
+app.get("/", function(req, res){
+  res.send("<h1>Hello, World!</h1>");
+});
+app.get("/contact", function(req,res){
+  res.send("Contact me at 123456");
+});
+app.get("/about", function(req,res){
+  res.send("My name is About ME");
 });
 
-app.get("/", function(req, res){
-  res.send("ok");
+
+// LISTEN
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
