d3541cc51bc3e7dfc598ff7d53378596f9327dae
Diky M <dmulyana+git@gmail.com>
2021-12-27 15:03:54 +0700

    new files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const https = require("https");
+
+const app = express();
+
+
+app.get("/", function(req,res) {
+    let url = "https://my-json-server.typicode.com/typicode/demo/posts";
+    res.send("Server is up");
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+    });
+});
+
+app.listen(3000, function() {
+    console.log("Server is running on Port 3000");
+});
