3a1341ca64e8ad0cdbc18174d5a69def4ea827a6
Diky M <dmulyana+github@gmail.com>
2021-12-30 11:51:30 +0700

    add more reqs

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require ("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
 
-app.get("/", function(req,res) {
+app.post("/", function(req,res) {
     let url = "https://my-json-server.typicode.com/typicode/demo/posts";
-    res.send("Server is up and runnning.");
+    // res.send("Server is up and runnning.");
+    console.log(req.body.cityName);
     https.get(url, function(response) {
         console.log(response.statusCode);
+
+        response.on("data", function(data){
+            let weatherData = JSON.parse(data);
+            let temp = weatherData[0].id;
+            let weatherDescription = weatherData[0].title;
+            console.log(temp + " " + weatherDescription);
+            res.send("<h1>The temp in London is " + temp + " degrees Celcius and " + weatherDescription + "</h1>");
+        });
     });
 });
 
